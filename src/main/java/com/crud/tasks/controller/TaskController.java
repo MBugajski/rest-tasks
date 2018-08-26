57c5d1c4eb15c67ae41b02a699decaece3fe6306
MBugajski <>
2018-08-26 18:24:34 +0200

    Basic service, mapper and repository implementation

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,27 +1,31 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/vi/task")
+@RequestMapping("/v1/task")
 public class TaskController {
-//    private TaskMapper taskMapper;
-//    private DBService dbService;
+    @Autowired
+    private TaskMapper taskMapper;
+    @Autowired
+    private DbService service;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask() {
-        return new TaskDto(1L, "test title", "test content");
+    public TaskDto getTask(Long id) {
+        return taskMapper.mapToTaskDto(service.getOneTask(id));
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
